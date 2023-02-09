41ff029b684977208cfad8c64e3bf503eb57760c
kvanitp2 <=>
2023-02-09 10:18:35 +0500

    aim menu

---
+++ main.js
@@ -0,0 +1,14 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('time-btn')) {
+        screens[1].classList.add('up')
+    }
+})
